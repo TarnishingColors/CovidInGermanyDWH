c88e8a57242244922017fee37733e9c39d810c3f
agabitashvili <alex.gabitas@gmail.com>
2023-07-20 15:14:24 +0200

    add vaccinations ods

--- ods/states_vaccinations/states_vaccinations.py
+++ ods/states_vaccinations/states_vaccinations.py
@@ -139,7 +139,6 @@ result_df = union_multiple_dfs([
     states_fourth_booster_vaccinations
 ])
 
-
 load_table = Table(schema='default', table_name='states_vaccinations', periodic_column='load_date')
 hl = HiveLoad(level=Level.ods, df=result_df, table=load_table, spark=ext.spark)
 hl.load_by_period()
---
+++ ods/vaccinations/vaccinations.py
@@ -0,0 +1,36 @@
+from datetime import datetime, timedelta
+
+from pipeline.extract import S3Extract
+from pipeline.load import Table, HiveLoad
+from pipeline.utils import Level
+from pyspark.sql import functions as F
+
+previous_date = str((datetime.today() - timedelta(1)).date())
+ext = S3Extract("ods_vaccinations")
+df = ext.extract(file_dir=f"raw/default/vaccinations_{previous_date}.json")
+
+result_df = (
+    df.selectExpr(
+        "EXPLODE(data.history) AS vaccinations_by_day",
+        "meta.source AS source",
+        "meta.info AS info",
+        "TO_DATE(meta.lastUpdate) AS last_update_date"
+    )
+    .selectExpr(
+        "*",
+        "TO_DATE(vaccinations_by_day.date) AS vaccination_date",
+        "vaccinations_by_day.vaccinated",
+        "vaccinations_by_day.firstVaccination AS first_vaccination",
+        "vaccinations_by_day.secondVaccination AS second_vaccination",
+        "vaccinations_by_day.firstBoosterVaccination AS first_booster_vaccination",
+        "vaccinations_by_day.secondBoosterVaccination AS second_booster_vaccination",
+        "vaccinations_by_day.thirdBoosterVaccination AS third_booster_vaccination",
+        "vaccinations_by_day.fourthBoosterVaccination AS fourth_booster_vaccination",
+        "vaccinations_by_day.totalVacciantionOfTheDay AS total_vacciantion_by_day",
+    )
+    .drop("vaccinations_by_day")
+).fillna(0)
+
+load_table = Table(schema='default', table_name='vaccinations', periodic_column='vaccination_date')
+hl = HiveLoad(level=Level.ods, df=result_df, table=load_table, spark=ext.spark)
+hl.load_by_period()
